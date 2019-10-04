79e5a86f39c01d0054c4212131106c1d1146bc28
CruiseDevice <meakashchavan@gmail.com>
2019-10-04 12:45:51 +0530

    Add dev.env.js
    - Consists of validate URL and Token.

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,8 +2,8 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
-const validate_url = "API_URL"
-const token = "API_TOKEN" //"d88812ee582e2ab6bb0cdd93faa600541a2bcb39"
+const validate_url = '"http://localhost:8000/api/validate/"'
+const token = '8ebb5a54048458a8bd73da259391a092c6627f1d' //"d88812ee582e2ab6bb0cdd93faa600541a2bcb39"
 
 
 module.exports = merge(prodEnv, {
